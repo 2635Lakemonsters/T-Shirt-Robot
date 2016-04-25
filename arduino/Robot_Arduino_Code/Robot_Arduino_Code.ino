0229b404479e49bd835638d48d3245c589743721
Matthew Martin <matthew.r.martin@me.com>
2016-04-25 16:41:11 -0700

    Added arduino sketch to repository.

---
+++ arduino/Robot_Arduino_Code/Robot_Arduino_Code.ino
@@ -0,0 +1,138 @@
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+//Based heavily off of 3574's LED Arduino code, modified and appended by 2635
+
+void setup() 
+{
+  strip.begin();
+  strip.show();
+  Wire.being(4);
+  Wire.onReceive(receiveEvent);
+}
+
+int showPattern = -1;
+uint32_t colorRed = strip.Color(255,0,0);
+
+void loop() 
+{
+  showPattern = 0;
+  switch(showPattern)
+  {
+   case 0: rainbowCycle(5); showPattern = -1; break;
+   case 1: colorWipe(colorRed, 10); showPattern = -1; break;
+   case 2: marchRedWhiteBlue(); showPattern = -1; break;
+  default:    solidColor(strip.Color(0, 0, 0));
+  strip.show(); showPattern = -1; break;
+  }
+
+}
+
+
+//Wipe a color LED by LED through the strip, then clear it. Wait is delay between LED setting.
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+
+  for(uint16_t i=0; i<strip.numPixels(); i++) 
+    {
+      strip.setPixelColor(i, strip.Color(0,0,0));
+      strip.show();
+      delay(wait);
+    }
+}
+
+
+void marchRedWhiteBlue()
+{
+  uint32_t colors[] = { strip.Color(255,0,0), strip.Color(0,0,255), strip.Color(155,155,155) };
+  // Turn everything black
+  solidColor(strip.Color(0, 0, 0));
+  
+  int frame = 0;
+  
+  while( frame < 50 ) {
+    for (int index = 0;index<strip.numPixels();index++)
+    {
+      //strip.setPixelColor((index  + (frame % 15)) % 60, colors[ ((index)/5 + frame) % 3 ]);
+      strip.setPixelColor((index + frame) % 60 , colors[ ((index)/5) % 3 ]);
+    }
+    strip.show();
+    delay(50);
+    frame++;
+  }
+  
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+
+
+//UTIL METHODS. Call them if you want. See if I care.
+
+void solidColor(uint32_t c){
+    for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      
+    }
+}
+
+void receiveEvent(int howMany)
+{
+  static uint8_t ct = 0;
+  int x = Wire.read();
+  showPattern = x;
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+// Run the length of the strip one light at a time
+void colorSwipe(uint32_t color, uint8_t wait) {
+  for(int i = 0; i <= strip.numPixels(); i++) {
+    // going out of bounds does not break things, so checks removed for readability
+    // clean up behind it
+    strip.setPixelColor(i-1, 0); 
+    //if (i < strip.numPixels()) 
+    strip.setPixelColor(i, color);
+    strip.show();
+    delay(wait);
+  }
+}
+
+
